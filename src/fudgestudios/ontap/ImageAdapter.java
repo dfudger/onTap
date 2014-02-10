66b01ba961f85b6d4077d6b4526a50d5914caa78
dfudger <dfudger@uoguelph.ca>
2014-02-10 10:23:46 -0500

    Working on the gallery view bug. Thumbnails are now really small. Fixing ImageAdapter class

--- src/fudgestudios/ontap/ImageAdapter.java
+++ src/fudgestudios/ontap/ImageAdapter.java
@@ -8,35 +8,58 @@ import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter 
 {
-    private Context mContext;
-    private ArrayList<String> urlArray = new ArrayList<String>();
-
+    
+//EXAMPLE
+	private Context mContext;
+	private ArrayList<String> urlArray = new ArrayList<String>();
+	
+    // Constructor
     public ImageAdapter(Context c, ArrayList <String> a) {
         mContext = c;
         urlArray = a;
     }
-
-	@Override
+ 
+    @Override
 	public int getCount() {
         return urlArray.size();
     }
-
+ 
+    //@Override MINE
+    //public Object getItem(int position) {
+    //   return mThumbIds[position];
+    //}
+    
     @Override
 	public Object getItem(int position) {
         return null;
     }
-
+ 
     @Override
 	public long getItemId(int position) {
         return 0;
     }
-    
-    // create a new ImageView for each item referenced by the Adapter
+ 
     @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        //imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+        
+        Bitmap myBitmap = BitmapFactory.decodeFile(urlArray.get(position));
+        imageView.setImageBitmap(myBitmap);
+        
+        return imageView;
+    }
+    
+    
+ // create a new ImageView for each item referenced by the Adapter
+  /*  @Override
 	public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
@@ -52,5 +75,7 @@ public class ImageAdapter extends BaseAdapter
         imageView.setImageBitmap(myBitmap);
         
         return imageView;
-    }
+    }*/
+    //END EXAMPLE
+
 }
